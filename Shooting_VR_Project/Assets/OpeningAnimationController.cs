2a903bbb821b25d75e1595def048c3a57cbcbdb8
Unknown <taka84re@bd5.so-net.ne.jp>
2019-09-03 14:18:32 +0900

    一部音追加

--- Shooting_VR_Project/Assets/OpeningAnimationController.cs
+++ Shooting_VR_Project/Assets/OpeningAnimationController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OpeningAnimationController : MonoBehaviour
 {
@@ -11,8 +12,6 @@ public class OpeningAnimationController : MonoBehaviour
     float time;
     float gateOpenTime = 6.0f;
     float lightUpTime = 8.0f;
-    float engineRunningTime = 9.0f;
-    float playerLaunchedTime = 12.0f;
     Renderer _renderer;
 
     // Start is called before the first frame update
@@ -25,23 +24,26 @@ public class OpeningAnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //アニメーションのタイミングを管理
         time += Time.deltaTime;
+
+        //ゲートが開くアニメーション
         if (time >= gateOpenTime)
         {
             gateTopAnim.enabled = true;
             gateBottomAnim.enabled = true;
         }
+
+        //ライトを点灯 コルーチンによる遅延処理
         if (time >= lightUpTime)
         {
             StartCoroutine(LightDelayMethod());
         }
-        if (time >= engineRunningTime)
-        {
 
-        }
-        if (time >= 8.0f)
+        //チュートリアルシーンへ遷移
+        if (time >= 19.0f)
         {
-            //PB.enabled = true;
+            //SceneManager.LoadScene("Tutorial");
         }
     }
 
@@ -49,12 +51,16 @@ public class OpeningAnimationController : MonoBehaviour
     {
         for (int i = 0; i < (light.Count / 2); i++)
         {
+            //手前から(Light(*)の*の値が小さい)順にライトを点灯させる(Emissionのcolorを設定)
             _renderer = light[i * 2].GetComponent<Renderer>();
             _renderer.material.EnableKeyword("_EMISSION");
             _renderer.material.SetColor("_EmissionColor", new Color(191, 191, 75));
+
             _renderer = light[i * 2 + 1].GetComponent<Renderer>();
             _renderer.material.EnableKeyword("_EMISSION");
             _renderer.material.SetColor("_EmissionColor", new Color(191, 191, 75));
+
+            //0.1秒の遅延して返す
             yield return new WaitForSeconds(0.1f);
         }
     }
---
+++ Shooting_VR_Project/Assets/OpeningSoundController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningSoundController : MonoBehaviour
+{
+    float time;
+    float gateOpenTime = 6.0f;
+    float lightUpTime = 8.0f;
+    float engineRunningTime = 10.0f;
+    float playerLaunchedTime = 14.0f;
+
+    public AudioClip[] audioClips;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //タイミングを管理
+        time += Time.deltaTime;
+
+        //エレベーター上昇音
+        if (time >= 1.0f && time <= 1.1f)
+        {
+            audioSource.PlayOneShot(audioClips[0]);
+        }
+
+        //ゲートが開く音
+        if (time >= gateOpenTime && time <= gateOpenTime + 0.1f)
+        {
+            audioSource.PlayOneShot(audioClips[1]);
+        }
+
+        //ライト点灯
+        if (time >= lightUpTime)
+        {
+            audioSource.PlayOneShot(audioClips[2]);
+        }
+
+        //エンジンブースト音
+        if (time >= engineRunningTime && time <= engineRunningTime + 0.1f)
+        {
+            audioSource.PlayOneShot(audioClips[3]);
+        }
+
+        //出撃する際の音
+        if (time >= playerLaunchedTime && time <= playerLaunchedTime + 0.1f)
+        {
+            audioSource.PlayOneShot(audioClips[4]);
+        }
+    }
+}
