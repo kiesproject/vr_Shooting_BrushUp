f0c46f8c5ba6da25052cbcc5b3670b9649c6daf7
Unknown <b1704736@planet.kanazawa-it.ac.jp>
2019-10-17 20:05:21 +0900

    UIなどの調整

--- Shooting_VR_Project/Assets/Scripts/Message_launch.cs
+++ Shooting_VR_Project/Assets/Scripts/Message_launch.cs
@@ -16,17 +16,10 @@ public class Message_launch : MonoBehaviour
 
     private string[] Messages =
     {
-        "宇宙戦闘機spicaの発射を確認/味方補給艦隊により敵戦闘機群及び大型兵器を確認。目標地点に向け進行中\n目標：敵艦隊の殲滅\n/当機のラーニングプログラムを実行/指示に従い、機体を操作してください/",
-        "当機体の操縦方法を説明します/",
-        "当機はセミオート操縦により自動的に目的地へ航行します。\n右のコントローラーを立て、左右に傾けることで旋回します/奥に傾けると下降し、手前に傾けると上昇します/",
-        "前方より小惑星帯が接近中\nコントローラーを操作し、衝突を回避してください/",
-        "次は搭載武器の使用方法について説明します/右のコントローラーで照準を定め、トリガーを引くことでレーザー光線を発射します/",
-        "敵の無人戦闘機体を確認\nレーザー光線で迎撃してください/",
-        "敵の戦闘機体を複数確認\nミサイルを使用することで、敵を一掃することができます/左コントローラーのトリガーで武器を切り替えます\n左モニターの表示がMISSILEと表記されたことを確認してください/",
-        "右コントローラーのトリガーを引いて発射します/",
-        "正面にある緑色のゲージは当機のエネルギーです/ゲージがなくなると当機は推進力を失い、制御不能となります/",
-        "ラーニングプログラムの工程は以上となります/60秒後目標地点に到達\n敵艦隊の制圧に向け準備してください/",
-        ""
+        "EMERGENCY 緊急事態、味方補給艦隊が敵戦闘機群を確認\n現在交戦中、目的地はコロニーの残骸付近/",
+        "君の任務は敵勢力の掃討及び、敵巨大兵器の討伐だ/",
+        "敵巨大兵器の情報は皆無だ、十分に気をつけて出撃せよ/",
+        "健闘を祈る/"
     };
 
     private float t;
@@ -50,24 +43,15 @@ public class Message_launch : MonoBehaviour
     // テキストスピード
     [HideInInspector]
     [SerializeField]
-    private float textSpeed = 0.05f;
+    private float textSpeed = 4.0f;
     // テキストスピードに対し、メッセージの1つ1つの文字を表示する指標となる(経過)時間
     // タスクごとのTransitionTimeに対し、次のメッセージへと切り替える指標となる(経過)時間
-    private float elapsedTime = 0f;
+    private float elapsedTime = 6f;
     // 1回分のメッセージを表示したかどうか(messageNumが増加あるいは0にリセットされる直前にtrue)
     private bool isOneMessage = false;
     // メッセージをすべて表示したかどうか(nowTaskNumが増加する直前にtrue)
     private bool isEndMessage = false;
 
-    public GameObject player;
-    public GameObject astroids;
-    public GameObject[] enemys;
-
-    AirFighter_Controller AC;
-    float speed;
-
-    public AudioClip messageSound;
-    AudioSource audioSource;
 
     bool test = false;
 
@@ -77,10 +61,6 @@ public class Message_launch : MonoBehaviour
         messageText = GetComponentInChildren<Text>();
         messageText.text = "";
         SetMessage(Messages[taskNum], taskNum);
-        audioSource = GetComponent<AudioSource>();
-        AC = player.GetComponent<AirFighter_Controller>();
-        speed = AC.airFighter_speed;
-        GameManager.instance.Player_Update();
     }
 
     // Update is called once per frame
@@ -88,9 +68,14 @@ public class Message_launch : MonoBehaviour
     {
 
         // 全てのチュートリアルタスクを終了したら、シーン遷移
+        if (taskNum == Messages.Length - 1)
+        {
+            Destroy(this.gameObject);
+        }
 
-        //テスト用 PlayerBaseとAsteroidsの座標
-        if (test)
+
+            //テスト用 PlayerBaseとAsteroidsの座標
+            if (test)
         {
             //Debug.Log(player.transform.position);
             //Debug.Log(astroids.transform.position);
@@ -128,7 +113,6 @@ public class Message_launch : MonoBehaviour
             if (elapsedTime >= textSpeed)
             {
                 messageText.text += splitMessage[messageNum][nowTextNum];
-                audioSource.PlayOneShot(messageSound);
 
                 nowTextNum++;
                 elapsedTime = 0f;
@@ -162,49 +146,5 @@ public class Message_launch : MonoBehaviour
         isEndMessage = false;
     }
 
-    void TaskEvent()
-    {
-        switch (taskNum)
-        {
-            case 2:
-                t = TransitionTime[1];
-                break;
-            case 3:
-                t = TransitionTime[2];
-                //test = true;
-                break;
-            case 5:
-                t = TransitionTime[3];
-                try
-                {
-                    enemys[0].gameObject.SetActive(true);
-                }
-                catch { }
-                break;
-            case 6:
-                t = TransitionTime[0];
-                try
-                {
-                    for (int i = 1; i < enemys.Length; i++)
-                    {
-                        enemys[i].gameObject.SetActive(true);
-                    }
-                }
-                catch { }
-                break;
-            case 7:
-                t = TransitionTime[1];
-                break;
-            default:
-                t = TransitionTime[0];
-                break;
-        }
-    }
-
-    // 他のスクリプトから新しいメッセージを設定しUIをアクティブにする
-    //public void SetMessagePanel(string message)
-    //{
-    //SetMessage(message);
-    //transform.GetChild(0).gameObject.SetActive(true);
-    //}
+   
 }
